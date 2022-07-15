ba268adcc0af59ed8a90eee0c22cddb54ebd43f5
hi337 <ajamiehrateb@gmail.com>
2022-07-15 17:20:37 -0600

    main page done. Links and booking page on the way

--- components/Footer.js
+++ components/Footer.js
@@ -5,27 +5,27 @@ import styles from "../styles/Home.module.css";
 export default function Footer() {
   return (
     <div>
-      <div style={{ backgroundColor: "#f6b60b", marginTop: 30 }}>
-        <Container className={styles.footer} maxWidth="lg">
+      <div style={{ backgroundColor: "#f6b60b", marginTop: 30, paddingTop: 20, paddingBottom: 20}}>
+        {/* <Container className={styles.footer} maxWidth={"xl"}> */}
           <div className={styles.prefooter}>
-            <div>
-              <h5>Come now for flu and travel shots!</h5>
-              <h5>
+            <div style={{marginRight: 100}}>
+              <h4 className={styles.footerpretitle}>Come now for flu and travel shots!</h4>
+              <p className={styles.footerpretext}>
                 If you are planning to travel outside of the country, make sure
                 to get professional advise on recommended vaccinations!{" "}
-              </h5>
+              </p>
             </div>
-            <Button>Contact Us Now!</Button>
+            <Button className={styles.footerprebutton} variant={"contained"}><b>Contact Us</b></Button>
           </div>
-        </Container>
+        {/* </Container> */}
       </div>
       <div style={{backgroundColor: "#0d5d4d"}}>
         <Container maxWidth="lg">
         <div className={styles.mainfooter}>
-        <h4>Miracare Pharmacy</h4>
-        <p>Visit us today for all your medical needs</p>
+        <h2 style={{color: "#f6b60b"}}>Miracare Pharmacy</h2>
+        <p style={{color: "white"}}>Visit us today for all your medical needs</p>
         <div className={styles.footerbuttons}>
-            <IconButton><FaEnvelope size={28}/></IconButton>
+            <IconButton style={{color: 'white'}}><FaEnvelope size={28}/></IconButton>
         </div>
       </div>
         </Container>
